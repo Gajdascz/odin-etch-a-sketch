5c63a1c41e509b3230f90cce33421b93b5ebcd29
Nolan Gajdascz <78930672+Gajdascz@users.noreply.github.com>
2023-10-03 18:50:24 -0400

    Create draw and change dimension logic
    Added the logic to color grid squares on hover as well as the
    change dimensions functionality. Also, I fixed the grid sizing
    to fill the grid container no matter how many grid squares are
    present.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,41 @@
-( function () {
+function populateGrid(dimensions=16) {
   const gridContainer = document.querySelector(`.grid-container`);
-  for(let i = 0; i < 16; i++) {
+  for(let i = 0; i < dimensions; i++) {
     let gridRow = document.createElement(`div`);
-    gridRow.classList.toggle(`grid-row`);
+    gridRow.classList.add(`grid-row`);
     gridContainer.appendChild(gridRow);
-    for( let j = 0; j < 16; j++){
+    for( let j = 0; j < dimensions; j++) {
       let gridSquare = document.createElement(`div`);
-      gridSquare.classList.toggle(`grid-square`);
-      gridRow.appendChild(gridSquare)
+      gridSquare.classList.add(`grid-square`);
+      gridRow.appendChild(gridSquare);
     }
+  }
+  applyGridSquareListeners();
 }
+
+function changeGridDimensions(newDimensions){
+  const gridContainer = document.querySelectorAll(`.grid-container > *`);
+  gridContainer.forEach((Node) => {
+    Node.remove();
+  });
+  populateGrid(newDimensions);
+}
+
+function applyGridSquareListeners() {
+  const gridSquare = document.querySelectorAll(`.grid-square`);
+  gridSquare.forEach((gridSquare) => {
+    gridSquare.addEventListener(`mouseover`, () => {
+      gridSquare.classList.add(`colored-grid-square`);
+    });
+  });
+}
+
+(function () {
+  const changeDimensionsButton = document.querySelector(`.change-dimensions-button`);
+  changeDimensionsButton.addEventListener(`click`, () => {
+    let newDimensions = prompt(`Enter a number less than 100 to change the grid dimensions.`);
+    changeGridDimensions(newDimensions);
+  });
 })();
+
+populateGrid();
